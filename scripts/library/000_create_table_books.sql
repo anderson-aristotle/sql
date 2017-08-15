7c1d6f59ce8273fd2f317bce066580b3b8edfba5
Yan Chen <c.yan_92@yahoo.com>
2017-08-15 15:05:09 -0400

    Revert "Merge pull request #40 from ga-wdi-boston/019/solution"
    This reverts commit 95f9df142184a6810e287e6c5ab17e1670b953ad, reversing
    changes made to ac2314995fb6a2d75a25d05be54cfbf4f3f1552c.

--- scripts/library/000_create_table_books.sql
+++ scripts/library/000_create_table_books.sql
@@ -1,8 +1,1 @@
 -- create a table to store information about books
-CREATE TABLE books (
-  id SERIAL PRIMARY KEY,
-  title TEXT,
-  author TEXT,
-  original_language TEXT,
-  first_published INTEGER
-);
